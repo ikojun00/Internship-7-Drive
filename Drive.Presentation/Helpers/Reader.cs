6f0daa1bd142fc233bbeeee845439a84ed19c4f7
Ivor <ikojun00@fesb.hr>
2025-01-03 15:59:47 +0100

    implementing helper functions

---
+++ Drive.Presentation/Helpers/Reader.cs
@@ -0,0 +1,17 @@
+﻿namespace Internship_7_Drive.Helpers
+{
+    public class Reader
+    {
+        public static bool TryReadConfirmation(out bool confirmed)
+        {
+            confirmed = false;
+            var input = Console.ReadLine()?.ToLower();
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            confirmed = input == "yes";
+            return true;
+        }
+    }
+}
---
+++ Drive.Presentation/Helpers/Writer.cs
@@ -0,0 +1,20 @@
+﻿namespace Internship_7_Drive.Helpers
+{
+    public class Writer
+    {
+        public static void WriteInvalidCommand(string expectedFormat)
+        {
+            Console.WriteLine($"Invalid command format. Use: {expectedFormat}");
+        }
+
+        public static void WriteConfirmation(string itemType, string name, string action)
+        {
+            Console.Write($"{action} {itemType} '{name}'? (yes/no): ");
+        }
+
+        public static void WriteCancellation(string itemType, string name, string action)
+        {
+            Console.WriteLine($"{action} {itemType} '{name}' cancelled.");
+        }
+    }
+}
